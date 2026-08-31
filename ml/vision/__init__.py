3472b452d81d325e391f43ee77795651bbea7c7e
Semedw <smusazade2000@gmail.com>
2026-08-31 23:34:05 +0400

    feat: add classification result types and confidence filter

--- ml/vision/__init__.py
+++ ml/vision/__init__.py
@@ -1,4 +1,5 @@
 from ml.vision.background import BackgroundRemovalError, remove_background
+from ml.vision.classify import ClassifiedItem, filter_by_confidence
 from ml.vision.color import (
     ColorCluster,
     ColorSpec,
@@ -10,6 +11,8 @@ from ml.vision.color import (
 __all__ = [
     "remove_background",
     "BackgroundRemovalError",
+    "ClassifiedItem",
+    "filter_by_confidence",
     "ColorCluster",
     "ColorSpec",
     "dominant_color",
