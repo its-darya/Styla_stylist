994cf6fa821015135b2d46807c3d11525ba569da
Semedw <smusazade2000@gmail.com>
2026-08-31 23:27:00 +0400

    feat: add K-means dominant color extraction

--- ml/vision/__init__.py
+++ ml/vision/__init__.py
@@ -1,3 +1,18 @@
 from ml.vision.background import BackgroundRemovalError, remove_background
+from ml.vision.color import (
+    ColorCluster,
+    ColorSpec,
+    dominant_color,
+    extract_dominant_colors,
+    rgb_to_hsv_spec,
+)
 
-__all__ = ["remove_background", "BackgroundRemovalError"]
+__all__ = [
+    "remove_background",
+    "BackgroundRemovalError",
+    "ColorCluster",
+    "ColorSpec",
+    "dominant_color",
+    "extract_dominant_colors",
+    "rgb_to_hsv_spec",
+]
